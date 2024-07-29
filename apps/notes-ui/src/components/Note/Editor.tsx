6078b96445b7e45b6ede6a7d29b57e355370123d
Griffin <griffin@yardzen.com>
2024-07-29 19:15:21 -0400

    Wrapping up the editor

--- apps/notes-ui/src/components/Note/Editor.tsx
+++ apps/notes-ui/src/components/Note/Editor.tsx
@@ -1,16 +1,22 @@
 import { Note } from "@prisma/client";
 
 import StarterKit from "@tiptap/starter-kit";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import TextAlign from "@tiptap/extension-text-align";
+import Dropcursor from "@tiptap/extension-dropcursor";
+import Link from '@tiptap/extension-link'
+import Image from '@tiptap/extension-image'
 import { TableCell } from "@tiptap/extension-table-cell";
 import { TableHeader } from "@tiptap/extension-table-header";
 import { TableRow } from "@tiptap/extension-table-row";
+import type { EditorOptions } from "@tiptap/core";
 import {
   MenuButtonAddTable,
   MenuButtonBold,
   MenuButtonBulletedList,
   MenuButtonCode,
   MenuButtonCodeBlock,
-  MenuButtonHorizontalRule,
   MenuButtonItalic,
   MenuButtonOrderedList,
   MenuButtonRedo,
@@ -18,15 +24,26 @@ import {
   MenuControlsContainer,
   MenuDivider,
   MenuSelectHeading,
-  RichTextEditorProvider,
-  RichTextField,
   TableBubbleMenu,
   TableImproved,
   RichTextEditor,
+  MenuButtonEditLink,
+  MenuButtonTaskList,
+  MenuButtonAlignCenter,
+  MenuButtonAlignLeft,
+  MenuButtonAlignRight,
+  LinkBubbleMenu,
+  LinkBubbleMenuHandler,
+  MenuButtonAddImage,
+  ResizableImage,
   type RichTextEditorRef,
+  useRichTextEditorContext,
+  RichTextEditorProvider,
+  insertImages
 } from "mui-tiptap";
 import { useCallback, useRef, useState } from "react";
 import { Editor as TiptapEditor } from "@tiptap/core";
+import { useEditor } from "@tiptap/react";
 
 interface Props {
   note: Note,
@@ -39,6 +56,12 @@ const extensions = [
   TableCell,
   TableHeader,
   TableRow,
+  TaskList,
+  TaskItem,
+  TextAlign,
+  Link,
+  ResizableImage,
+  LinkBubbleMenuHandler,
   TableImproved.configure({
     resizable: true,
   }),
@@ -59,6 +82,10 @@ export function Editor({
   const [lastSaveAt, setLastSaveAt] = useState<Date | null>(null);
   const lastSaveAtRef = useRef<Date | null>(null);
   lastSaveAtRef.current = lastSaveAt;
+  const editor = useEditor({
+    extensions: extensions,
+    content: note.content,
+  });
 
   const handleUpdate = ({ editor }: { editor: TiptapEditor }) => {
     setLastSaveAt(new Date());
@@ -87,27 +114,156 @@ export function Editor({
     }, SAVE_TIMEOUT);
   }, [])
 
+  const fileListToImageFiles = (fileList: FileList): File[] => {
+    // You may want to use a package like attr-accept
+    // (https://www.npmjs.com/package/attr-accept) to restrict to certain file
+    // types.
+    return Array.from(fileList).filter((file) => {
+      const mimeType = (file.type || "").toLowerCase();
+      return mimeType.startsWith("image/");
+    });
+  }
+
+  const handleNewImageFiles = useCallback(
+    (files: File[], insertPosition?: number): void => {
+      if (!rteRef.current?.editor) {
+        return;
+      }
+
+      // For the sake of a demo, we don't have a server to upload the files to,
+      // so we'll instead convert each one to a local "temporary" object URL.
+      // This will not persist properly in a production setting. You should
+      // instead upload the image files to your server, or perhaps convert the
+      // images to bas64 if you would like to encode the image data directly
+      // into the editor content, though that can make the editor content very
+      // large. You will probably want to use the same upload function here as
+      // for the MenuButtonImageUpload `onUploadFiles` prop.
+      const attributesForImageFiles = files.map((file) => ({
+        src: URL.createObjectURL(file),
+        alt: file.name,
+      }));
+
+      insertImages({
+        images: attributesForImageFiles,
+        editor: rteRef.current.editor,
+        position: insertPosition,
+      });
+    },
+    [],
+  );
+
+  const handlePaste: NonNullable<EditorOptions["editorProps"]["handlePaste"]> =
+    useCallback(
+      (_view, event, _slice) => {
+        if (!event.clipboardData) {
+          return false;
+        }
+
+        const pastedImageFiles = fileListToImageFiles(
+          event.clipboardData.files,
+        );
+        if (pastedImageFiles.length > 0) {
+          handleNewImageFiles(pastedImageFiles);
+          // Return true to mark the paste event as handled. This can for
+          // instance prevent redundant copies of the same image showing up,
+          // like if you right-click and copy an image from within the editor
+          // (in which case it will be added to the clipboard both as a file and
+          // as HTML, which Tiptap would otherwise separately parse.)
+          return true;
+        }
+
+        // We return false here to allow the standard paste-handler to run.
+        return false;
+      },
+      [handleNewImageFiles],
+    );
+
+  const handleDrop: NonNullable<EditorOptions["editorProps"]["handleDrop"]> =
+    useCallback(
+      (view, event, _slice, _moved) => {
+        if (!(event instanceof DragEvent) || !event.dataTransfer) {
+          return false;
+        }
+
+        const imageFiles = fileListToImageFiles(event.dataTransfer.files);
+        if (imageFiles.length > 0) {
+          const insertPosition = view.posAtCoords({
+            left: event.clientX,
+            top: event.clientY,
+          })?.pos;
+
+          handleNewImageFiles(imageFiles, insertPosition);
+
+          // Return true to treat the event as handled. We call preventDefault
+          // ourselves for good measure.
+          event.preventDefault();
+          return true;
+        }
+
+        return false;
+      },
+      [handleNewImageFiles],
+    );
+
   return (
-    <RichTextEditor
-      onUpdate={debouncedUpdate}
-      ref={rteRef}
-      extensions={extensions}
-      content={note.content}
-      renderControls={() => (
-        <MenuControlsContainer>
-          <MenuSelectHeading />
-          <MenuDivider />
-          <MenuButtonBold />
-          <MenuButtonItalic />
-          <MenuButtonAddTable />
-        </MenuControlsContainer>
-      )}
-    >
-      {() => (
-        <>
-          <TableBubbleMenu />
-        </>
-      )}
-    </RichTextEditor>
+    <RichTextEditorProvider editor={editor}>
+      <RichTextEditor
+        onUpdate={debouncedUpdate}
+        editorProps={{
+          handlePaste,
+          handleDrop,
+        }}
+        RichTextFieldProps={{
+          variant: "outlined",
+        }}
+        ref={rteRef}
+        extensions={extensions}
+        content={note.content}
+        renderControls={() => (
+          <MenuControlsContainer>
+            <MenuSelectHeading />
+            <MenuDivider />
+            <MenuButtonUndo />
+            <MenuButtonRedo />
+            <MenuDivider />
+            <MenuButtonBulletedList />
+            <MenuButtonOrderedList />
+            <MenuButtonTaskList />
+            <MenuDivider />
+            <MenuButtonBold />
+            <MenuButtonItalic />
+            <MenuDivider />
+            <MenuButtonCode />
+            <MenuButtonCodeBlock />
+            <MenuDivider />
+            <MenuButtonAddTable />
+            <MenuDivider />
+            <MenuButtonAlignLeft />
+            <MenuButtonAlignCenter />
+            <MenuButtonAlignRight />
+            <MenuDivider />
+            <MenuButtonEditLink />
+            <MenuDivider />
+            <MenuButtonAddImage
+              onClick={() => {
+                const url = window.prompt("Image URL");
+      
+                if (url) {
+                  editor?.chain().focus().setImage({ src: url }).run();
+                }
+              }}
+            />
+          </MenuControlsContainer>
+        )}
+        >
+        {() => (
+          <>
+            <LinkBubbleMenu />
+            <TableBubbleMenu />
+          </>
+        )}
+      </RichTextEditor>
+
+    </RichTextEditorProvider>
   )
 }
