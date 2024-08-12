f50820cb7a9ddc6227489bcbbcd58e4d618deef4
Griffin <griffin@yardzen.com>
2024-08-12 11:41:09 -0400

    Changes base client

--- apps/notes-ui/src/utils/api/baseClient.ts
+++ apps/notes-ui/src/utils/api/baseClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const baseClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
