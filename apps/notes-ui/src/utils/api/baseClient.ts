f3a15de2159bb7abad688ef58367b335c6fd6522
Griffin <griffin.mahoney@gmail.com>
2025-10-15 22:57:18 -0400

    API config

--- apps/notes-ui/src/utils/api/baseClient.ts
+++ apps/notes-ui/src/utils/api/baseClient.ts
@@ -4,7 +4,7 @@ import { urls } from '../../utils/urls';
 const isLocal = window.location.hostname === 'localhost';
 const baseURL = isLocal
   ? 'http://localhost:3000/api'
-  : 'https://notes.scriptmasterg.com/api';
+  : 'https://notes.griffinmahoney.me/api';
 
 export const baseClient = axios.create({
   baseURL,
