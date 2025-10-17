8062b8ad58dff17b0c7b82c706246bfc6efe1ca9
Griffin <griffin@yardzen.com>
2025-10-16 20:08:04 -0400

    fix

--- apps/ui2/tailwind.config.js
+++ apps/ui2/tailwind.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from '@tailwindcss/postcss'
-
-export default defineConfig({
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-})
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
