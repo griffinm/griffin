7c825a60556a03cecf3f889f481ba46bb3681ae6
Griffin <griffin.mahoney@gmail.com>
2025-11-11 12:41:05 -0500

    Enhance webpack configuration to ignore TypeScript type-only export warnings and simplify type exports in the types library.

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,12 +1,1 @@
-export type {
-  RequestWithUser,
-  CompletedFilterOptions,
-  PriorityOptionType,
-  SignInResponse,
-  JwtPayload,
-  CreateUserResponse,
-  SearchResult,
-  SearchResultQueryResult,
-} from './lib/types';
-
-export { PriorityOptions } from './lib/types';
+export * from './lib/types';
