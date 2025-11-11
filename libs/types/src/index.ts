13bcc82df3604fffaa64dc083e8415b81b9f6f1d
Griffin <griffin.mahoney@gmail.com>
2025-11-11 12:38:39 -0500

    Refactor type exports in index.ts to include specific types and PriorityOptions from lib/types

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -1,1 +1,12 @@
-export * from './lib/types';
+export type {
+  RequestWithUser,
+  CompletedFilterOptions,
+  PriorityOptionType,
+  SignInResponse,
+  JwtPayload,
+  CreateUserResponse,
+  SearchResult,
+  SearchResultQueryResult,
+} from './lib/types';
+
+export { PriorityOptions } from './lib/types';
